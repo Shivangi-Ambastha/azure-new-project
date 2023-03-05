7e48945e36f7d3d9ffff8681997e788fad29d099
Shivangi-Ambastha <125365273+Shivangi-Ambastha@users.noreply.github.com>
2023-03-05 21:43:21 +0530

    lets do it

--- locals.tf
+++ locals.tf
@@ -1,8 +1,6 @@
-locals {
 
-  aws_ecs_long_arn_commands = [
-    "aws ecs put-account-setting --name containerInstanceLongArnFormat --value enabled --region ${var.region}",
-    "aws ecs put-account-setting --name serviceLongArnFormat --value enabled --region ${var.region}",
-    "aws ecs put-account-setting --name taskLongArnFormat --value enabled --region ${var.region}"
-  ]
+#locals
+locals {
+  env             = terraform.workspace
+  require_keypair = (length(var.keypair_name) == 0) ? false : true
 }
